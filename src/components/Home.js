4bed90e1fd99192f4b17cdc997a85dcdf194e119
Gonzalo Iglesias <gonns.iglesias@gmail.com>
2021-05-15 19:24:01 -0300

    Initial project config (#1)
    * Initial react config
    
    * Added home page

---
+++ src/components/Home.js
@@ -0,0 +1,63 @@
+import { Button, Container, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
+    [theme.breakpoints.up("sm")]: {
+      height: "80vh",
+      minHeight: 500,
+      maxHeight: 1300,
+    },
+  },
+  container: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(14),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+  },
+}));
+
+const Home = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Container className={classes.container}>
+        <Typography color="inherit" align="center" variant="h3" marked="center">
+          Bienvenido al sistema de info sobre clases virtuales
+        </Typography>
+        <Button
+          className={classes.button}
+          color="primary"
+          variant="contained"
+        >
+          Entrar como universidad
+        </Button>
+        <Button
+          className={classes.button}
+          color="primary"
+          variant="contained"
+        >
+          Entrar como profesor
+        </Button>
+        <Button
+          className={classes.button}
+          color="primary"
+          variant="contained"
+        >
+          Entrar como alumno
+        </Button>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/theme.js
@@ -0,0 +1,5 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export default createMuiTheme({
+  spacing: 8,
+});
